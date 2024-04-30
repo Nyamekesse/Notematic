0878c829358f2a2dbd7aa2d9477cb9fead5fa629
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-04-30 13:25:36 +0000

    called text card component

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { TextCard } from 'components/TextCard/TextCard';
+import React from 'react';
 
 const NoteBrowse = () => {
-  return (
-    <div>NoteBrowse</div>
-  )
-}
+  return <TextCard title="Super Note" subtitle="01/02/23" content={'blbllblb blblbl lblblbl lblbl'} />;
+};
 
-export default NoteBrowse
+export default NoteBrowse;
