f69ac26f978f203d82cac4bf9c9298dd60163507
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-04-30 13:03:45 +0000

    added note slice o=to reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { noteReducer } from './noteSlice/note-slice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    NOTESLICE: noteReducer,
+  },
 });
