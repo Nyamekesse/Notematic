c56f948e1f647e225f98c97cfa91a688aa80da42
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-04-30 12:23:56 +0000

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import ButtonPrimary from 'components/ButtonPrimary/ButtonPrimary';
+import { Logo } from 'components/logo';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logoImg from '../../assets/images/logo.png';
+import style from './style.module.css';
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={`row ${style.container}`}>
+      <div className="col-xs-12 col-sm-4">
+        <Logo title="Notomatic" subtitle="Manage your notes" image={logoImg} onClick={() => navigate('/')} />
+      </div>
+      <div className="col-xs-12 col-sm-8 text-end">
+        <ButtonPrimary onClick={() => navigate('/note/new')}>Add note +</ButtonPrimary>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
