80bdb48e4f5b29542c92fd9ded260b73ccf41c0b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-06 22:24:19 +0000

    conditionally editing inputs based in is editable, toh=ggled is editable value per onclick

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import NoteForm from 'components/NoteForm/NoteForm';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const Note = () => {
+  const [isEditable, setIsEditable] = useState(false);
+  const { noteId } = useParams();
+  const note = useSelector((store) => store.NOTESLICE.noteList.find((note) => note.id === noteId));
+  const submit = () => {
+    alert('hey');
+  };
   return (
-    <div>Note</div>
-  )
-}
+    <>
+      {note && (
+        <NoteForm
+          title={isEditable ? 'Edit Note' : note.title}
+          note={note}
+          isEditable={isEditable}
+          onClickDelete={() => {}}
+          onClickEdit={() => {
+            setIsEditable(!isEditable);
+          }}
+          onSubmit={isEditable && submit}
+        />
+      )}
+    </>
+  );
+};
 
-export default Note
+export default Note;
