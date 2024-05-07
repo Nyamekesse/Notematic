529a59a4fc00d68868b038baba07053f1973736a
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-07 11:44:09 +0000

    implemeted on delete window, made sure to accept user confirmation

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -1,24 +1,31 @@
+import { NoteAPI } from 'api/note-api';
 import { TextCard } from 'components/TextCard/TextCard';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { deleteNote } from 'store/noteSlice/note-slice';
 import style from './style.module.css';
 
-const NoteList = () => {
+const NoteList = ({ noteList }) => {
   const navigate = useNavigate();
-  const { noteList } = useSelector((store) => store.NOTESLICE);
+  const dispatch = useDispatch();
+  async function deleteNote_(note) {
+    if (window.confirm('Delete Note?')) {
+      NoteAPI.deleteById(note.id);
+      dispatch(deleteNote(note));
+    }
+  }
   return (
     <div className={`row justify-content-center`}>
       {noteList.map((note, index) => {
         return (
-          <div className={style.card_container}>
+          <div className={style.card_container} key={index}>
             <TextCard
-              key={index}
               title={note.title}
               content={note.content}
               subtitle={note.created_at}
               onClick={() => navigate(`/note/${note.id}`)}
-              onClickTrash={() => {}}
+              onClickTrash={() => deleteNote_(note)}
             />
           </div>
         );
