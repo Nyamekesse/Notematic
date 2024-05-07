574399a77becaf4398a131ed21d5cdf866da0bdd
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-07 11:43:16 +0000

    added delete and update function

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,15 +1,29 @@
+import { NoteAPI } from 'api/note-api';
 import NoteForm from 'components/NoteForm/NoteForm';
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { deleteNote, updateNote } from 'store/noteSlice/note-slice';
 
 const Note = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isEditable, setIsEditable] = useState(false);
   const { noteId } = useParams();
   const note = useSelector((store) => store.NOTESLICE.noteList.find((note) => note.id === noteId));
-  const submit = () => {
-    alert('hey');
+  const submit = async (formValues) => {
+    const updatedNote = await NoteAPI.updateById(note.id, formValues);
+    dispatch(updateNote(updatedNote));
+    setIsEditable(false);
   };
+
+  async function deleteNote_() {
+    if (window.confirm('Delete Note?')) {
+      NoteAPI.deleteById(note.id);
+      dispatch(deleteNote(note));
+      navigate('/');
+    }
+  }
   return (
     <>
       {note && (
@@ -17,7 +31,7 @@ const Note = () => {
           title={isEditable ? 'Edit Note' : note.title}
           note={note}
           isEditable={isEditable}
-          onClickDelete={() => {}}
+          onClickDelete={deleteNote_}
           onClickEdit={() => {
             setIsEditable(!isEditable);
           }}
