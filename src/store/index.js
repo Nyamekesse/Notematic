9d0b4781d45a312e86214238bcea29ca8a2f86b6
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-19 23:09:20 +0000

    configured AUTHSLICE

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "./auth/auth-slice";
 import { noteReducer } from './noteSlice/note-slice';
 
 export const store = configureStore({
   reducer: {
     NOTESLICE: noteReducer,
+    AUTHSLICE: authReducer
   },
 });
