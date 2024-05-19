fd5a1eae4d63fe4aa56c575d3ab94fb9e363fa31
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-19 23:16:56 +0000

    setup firebase configuration

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCnpd-djyVN1GfwuCPAs7C1DpOgm9W_vbE',
+  authDomain: 'notamatic-c316d.firebaseapp.com',
+  projectId: 'notamatic-c316d',
+  storageBucket: 'notamatic-c316d.appspot.com',
+  messagingSenderId: '611216041010',
+  appId: '1:611216041010:web:9661027bf60fd84539482b',
+};
