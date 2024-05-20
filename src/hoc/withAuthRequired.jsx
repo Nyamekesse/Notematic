e2ed1653ace0c8e8a9668af7fb684cfc25a97b7b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-20 00:07:15 +0000

    created a higher order component to check if a user is authenticated before rendering pages, hence protecting pages from unauthenticated users

---
+++ src/hoc/withAuthRequired.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export const withAuthRequired = (Component) => {
+  return function ProtectedComponent() {
+    const navigate = useNavigate();
+    const { user } = useSelector((store) => store.AUTHSLICE.auth);
+    useEffect(() => {
+      if (!user) {
+        navigate('/signin');
+      }
+    }, []);
+
+    return user && <Component />;
+  };
+};
