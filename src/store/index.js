5bf5b5d83d7d6da72a56e73230e8d4a031d2f8e2
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-20 00:27:46 +0000

    setup redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,38 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/auth-slice";
 import { noteReducer } from './noteSlice/note-slice';
 
-export const store = configureStore({
-  reducer: {
-    NOTESLICE: noteReducer,
-    AUTHSLICE: authReducer
-  },
+
+import { combineReducers } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  NOTESLICE: noteReducer,
+  AUTHSLICE: authReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['AUTHSLICE'],
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };
+
+
+ 
