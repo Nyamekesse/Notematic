9a4d352692d3b18e9442b2d186be13cfad16d4bc
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-20 12:07:15 +0000

    moved sensitive configuration variables to .env file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCnpd-djyVN1GfwuCPAs7C1DpOgm9W_vbE',
-  authDomain: 'notamatic-c316d.firebaseapp.com',
-  projectId: 'notamatic-c316d',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: 'notamatic-c316d.appspot.com',
   messagingSenderId: '611216041010',
   appId: '1:611216041010:web:9661027bf60fd84539482b',
