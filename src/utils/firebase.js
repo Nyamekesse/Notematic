0e0430eb47bc56aec8e4040b9617364270b7edea
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-05-20 10:04:36 +0000

    updated configuration to add firestore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from '../config';
 
 export class FirebaseApp {
   static firebaseApp = undefined;
   static auth = undefined;
+  static db = undefined;
   static init() {
     this.firebaseApp = initializeApp(firebaseConfig);
     this.auth = getAuth();
+    this.db = getFirestore(this.firebaseApp);
   }
 }
